9440abd3ab2754e97a89c1f1e3be00a98d9c3031
Cannie511 <Canh51102@gmail.com>
2023-11-14 00:05:28 +0700

    sync nav

--- MusicProcess.php
+++ MusicProcess.php
@@ -1,37 +1,38 @@
 <?php
-function loadMusic(){
+function loadMusic()
+{
     $stt = 1;
-                for ($i = $stt; $i <= 20; $i++) {
-                    $stt = $i;
-                    $data = array(
-                        array("stt" => $stt, "img" => "hình", "name" => "tên bài hát", "singer" => "Ca sĩ", "date" => "1 tháng 1, 2023", "time" => "00:00")
-                    );
+    for ($i = $stt; $i <= 20; $i++) {
+        $stt = $i;
+        $data = array(
+            array("stt" => $stt, "img" => "hình", "name" => "tên bài hát", "singer" => "Ca sĩ", "date" => "1 tháng 1, 2023", "time" => "00:00")
+        );
 
-                    foreach ($data as $key => $music) {
-                        echo "<tr>";
-                        echo "<td>";
-                        print_r($data[$key]['stt']);
-                        echo "</td>";
-                        echo "<td>";
-                        print_r($data[$key]['img']);
-                        echo "</td>";
-                        echo "<td>";
-                        print_r($data[$key]['name']);
-                        echo "</td>";
-                        echo "<td>";
-                        print_r($data[$key]['singer']);
-                        echo "</td>";
-                        echo "<td>";
-                        print_r($data[$key]['date']);
-                        echo "</td>";
-                        echo "<td>";
-                        print_r($data[$key]['time']);
-                        echo "</td>";
-                        echo "<td>";
-                        echo "<ion-icon name='trash'></ion-icon>";
-                        echo "</td>";
-                        echo "</tr>";
-                    } 
-                }
+        foreach ($data as $key => $music) {
+            echo "<tr>";
+            echo "<td>";
+            print_r($data[$key]['stt']);
+            echo "</td>";
+            echo "<td>";
+            print_r($data[$key]['img']);
+            echo "</td>";
+            echo "<td>";
+            print_r($data[$key]['name']);
+            echo "</td>";
+            echo "<td>";
+            print_r($data[$key]['singer']);
+            echo "</td>";
+            echo "<td>";
+            print_r($data[$key]['date']);
+            echo "</td>";
+            echo "<td>";
+            print_r($data[$key]['time']);
+            echo "</td>";
+            echo "<td>";
+            echo "<ion-icon name='trash'></ion-icon>";
+            echo "</td>";
+            echo "</tr>";
+        }
+    }
 }
 ?>
--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 var navbar_toggle = document.getElementById("navbar");
 var menu = document.getElementById("menu");
 var all = document.querySelector("*");
@@ -7,61 +6,55 @@ var logoT = document.getElementById("logo_T");
 var logoV = document.getElementById("logo_V");
 var per = document.getElementById("personal");
 var more = document.getElementById("more");
-var mode = document.getElementById('scroll');
-var night = document.querySelector('body');
-var turn = document.getElementById('switch');
-var search = document.getElementById('search')
-var btn_play_light = document.getElementById('btn');
-var time_play = document.getElementById('time_play');
-var list_music = document.getElementById('list_mp3');
+var mode = document.getElementById("scroll");
+var night = document.querySelector("body");
+var turn = document.getElementById("switch");
+var search = document.getElementById("search");
+var btn_play_light = document.getElementById("btn");
+var time_play = document.getElementById("time_play");
+var list_music = document.getElementById("list_mp3");
 var logori = document.getElementById("logo_ri");
 var logoie = document.getElementById("logo_ie");
 var logo = document.getElementById("logo");
-
-navbar_toggle.onclick = function(){
-    navbar_toggle.classList.toggle('open');
-    menu.classList.toggle('hide');
-    main_play.classList.toggle('full');
-    logoT.classList.toggle('hide');
-    logoV.classList.toggle('hide');
-    logori.classList.toggle('hide');
-    logoie.classList.toggle('hide');
-    logo.classList.toggle('hide');
-    search.classList.toggle('full_scr');
-    list_music.classList.toggle('full_pg');
+var nav_rotate = document.getElementById("nav_rotate");
+navbar_toggle.onclick = function () {
+  navbar_toggle.classList.toggle("open");
+  menu.classList.toggle("hide");
+  main_play.classList.toggle("full");
+  logoT.classList.toggle("hide");
+  logoV.classList.toggle("hide");
+  logori.classList.toggle("hide");
+  logoie.classList.toggle("hide");
+  logo.classList.toggle("hide");
+  search.classList.toggle("full_scr");
+  list_music.classList.toggle("full_pg");
+};
+function play() {
+  var play = document.getElementById("play_btn");
+  var pause = document.getElementById("pause_btn");
+  play.style.display = "none";
+  pause.style.display = "block";
+  pause.style.position = "relative";
 }
-function play(){
-    var play = document.getElementById('play_btn');
-    var pause = document.getElementById('pause_btn');
-    play.style.display = 'none';
-    pause.style.display = 'block';
-    pause.style.position = 'relative';
+function pause() {
+  var play = document.getElementById("play_btn");
+  var pause = document.getElementById("pause_btn");
+  play.style.display = "block";
+  pause.style.display = "none";
 }
-function pause(){
-    var play = document.getElementById('play_btn');
-    var pause = document.getElementById('pause_btn');
-    play.style.display = 'block';
-    pause.style.display = 'none';
+function showMore() {
+  more.style.display = "block";
 }
-function showMore(){
-    more.style.display = 'block';
+function hideMore() {
+  more.style.display = "none";
 }
-function hideMore(){
-    more.style.display = 'none';
+mode.onclick = function nightMode() {
+  mode.classList.toggle("night");
+  night.classList.toggle("night_body");
+  turn.classList.toggle("night_on");
+  btn_play_light.classList.toggle("light_btn");
+  time_play.classList.toggle("light_time_play");
+};
+function log_in() {
+  window.location = "log-in.html";
 }
-mode.onclick = function nightMode(){
-    mode.classList.toggle('night');
-    night.classList.toggle('night_body');
-    turn.classList.toggle('night_on');
-    btn_play_light.classList.toggle('light_btn')
-    time_play.classList.toggle('light_time_play');
-}
-function log_in(){
-    window.location = "log-in.html";
-}
-
-
-
-
-
-
