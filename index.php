98e28fc305ca0f3d93f759b73fd67c51f6a071a6
Cannie511 <Canh51102@gmail.com>
2023-11-22 08:57:48 +0700

    sync all

--- index.php
+++ index.php
@@ -6,8 +6,10 @@
     <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
     <meta name="viewport" content="width=device-width, initial-scale=0.0">
     <title>TriVie Music</title>
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="index.css">
-
 </head>
 <?php include "MusicProcess.php" ?>
 <body>
