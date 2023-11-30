6c68db620996b42f84d1109aaa3d680a06649d0a
Cannie511 <Canh51102@gmail.com>
2023-11-30 11:37:11 +0700

    sync all

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=0.0">
     <title>TriVie Music</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
     <link rel="stylesheet" href="index.css">
     <script crossorigin src="https://unpkg.com/react@17/umd/react.development.js"></script>
     <script crossorigin src="https://unpkg.com/react-dom@17/umd/react-dom.development.js"></script>
@@ -17,14 +18,13 @@
 <body>
     <div class="noti">
             <div class="alert alert-success" id="alert" role="alert">
+            <i class="fa-solid fa-triangle-exclamation"></i>
                 A simple success alert—check it out!
             </div>
     </div>
     <div class="container-fluid">
-
         <div class="col-sm-4">
             <div id="menu">
-
                 <div id="logo" class="col-xl-12">
                     <h1>
                         <strong id="logo_T" class="col">T</strong>
@@ -42,33 +42,26 @@
                         <div class="menu_item" id="dsphat" data-bs-toggle="collapse" data-bs-target="#collapseOne"
                             aria-expanded="true" aria-controls="collapseOne"><ion-icon name="list-outline">
                             </ion-icon><span>DANH SÁCH PHÁT</span></div>
-                        <div id="collapseOne" class="accordion-collapse collapse " id="dropdownMenuOffset"
-                            data-bs-toggle="dropdown" aria-expanded="false" data-bs-offset="10,20">
-                            <div class="accordion-body">
-                                    <div class="col-md-4 menu_item" id=""><ion-icon
-                                            name="musical-notes"></ion-icon><span>DANH SÁCH 1</span></div>
-                            </div>
+                            <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                                <div class="col-md-4 menu_item" id="">
+                                    <ion-icon name="musical-notes"></ion-icon><span>DANH SÁCH 1</span>
+                                </div>
+                        </div>
+                        <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                                <div class="col-md-4 menu_item" id="">
+                                    <ion-icon name="musical-notes"></ion-icon><span>DANH SÁCH 2</span>
+                                </div>
+                        </div>
+                        <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                                <div class="col-md-4 menu_item" id="">
+                                    <ion-icon name="musical-notes"></ion-icon><span>DANH SÁCH 3</span>
+                                </div>
                         </div>
                         <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne"
                             data-bs-parent="#accordionExample">
-                            <div class="accordion-body">
-                                <div class="col-md-4 menu_item" id=""><ion-icon
-                                        name="musical-notes"></ion-icon><span>DANH SÁCH 2</span></div>
-                            </div>
-                        </div>
-                        <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne"
-                            data-bs-parent="#accordionExample">
-                            <div class="accordion-body">
-                                <div class="col-md-4 menu_item" id=""><ion-icon
-                                        name="musical-notes"></ion-icon><span>DANH SÁCH 3</span></div>
-                            </div>
-                        </div>
-                        <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne"
-                            data-bs-parent="#accordionExample">
-                            <div class="accordion-body">
                                 <div class="col-md-4 menu_item" id="" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@mdo">
-                                    <ion-icon name="add"></ion-icon><span>Tạo mới</span></div>
-                            </div>
+                                    <ion-icon name="add"></ion-icon><span>Tạo mới</span>
+                                </div>
                         </div>
                     </div>
                     <div class="col-md-4 menu_item" id="bxh"><ion-icon name="bar-chart-outline"></ion-icon><span>BẢNG
@@ -77,8 +70,20 @@
                     </div>
                     <div class="col-md-4 menu_item" id="yeu thich"><ion-icon name="heart-outline"></ion-icon><span>YÊU
                             THÍCH</span></div>
-                    <div class="col-md-4 menu_item" id="caidat"><ion-icon name="settings-outline"></ion-icon><span>CÀI
-                            ĐẶT</span></div>
+                            <div class="accordion" id="accordionExample">
+                        <div class="menu_item" data-bs-toggle="collapse" data-bs-target="#collapseOne1"
+                            aria-expanded="true" aria-controls="collapseOne"><ion-icon name="settings"></ion-icon><span>CÀI ĐẶT</span></div>
+                            <div id="collapseOne1" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                                <div class="col-md-4 menu_item" id="">
+                                <ion-icon name="lock-closed"></ion-icon><span>Đổi mật khẩu</span>
+                                </div>
+                            </div>
+                            <div id="collapseOne1" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                                <div class="col-md-4 menu_item" id="" data-bs-toggle="modal" data-bs-target="#ModalChangePass" >
+                                    <ion-icon name="pencil"></ion-icon><span>Chỉnh sửa tài khoản</span>
+                                </div>
+                            </div>
+                    </div>
                 </div>
                 <div id="navbar"><ion-icon name="chevron-back-outline"></ion-icon></div>
             </div>
@@ -110,6 +115,38 @@
                 </div>
             </div>
         </div>
+         <!-- --------------------------------------------------GIAO DIỆN ĐỔI MK-------------------------------------------------- -->
+         <div class="modal fade " id="ModalChangePass" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="exampleModalLabel">Đổi mật khẩu</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <form method="post">
+                        <div class="form-floating mb-3">
+                            <input type="password" class="form-control" id="floatingPassword" name="pass_user" placeholder="Mật khẩu cũ">
+                            <label for="floatingPassword">Mật khẩu cũ</label>
+                        </div>
+                        <div class="form-floating mb-3">
+                            <input type="password" class="form-control" id="floatingPassword" name="pass_user" placeholder="Mật khẩu mới">
+                            <label for="floatingPassword">Mật khẩu mới</label>
+                        </div>
+                        <div class="form-floating mb-3">
+                            <input type="password" class="form-control" id="floatingPassword" name="retype_pass_user" placeholder="Nhập lại mật khẩu mới">
+                            <label for="floatingPassword">Nhập lại mật khẩu mới</label>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Hủy</button>
+                            <input type="submit" class="btnAll primary" id = 'btnConfirm' value="Đổi mật khẩu">
+                        </div>
+                        </form>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
         <!-- --------------------------------------------ADVANCED LIST---------------------------------------------- -->
 
         <!-- --------------------------------------------ADVANCED LIST---------------------------------------------- -->
@@ -126,11 +163,11 @@
                                     ký</span></div>
                             <div onclick="log_in()"><ion-icon name="person"></ion-icon><span>Đăng nhập</span></div>
                             <div><ion-icon name="settings-sharp"></ion-icon><span>Cài đặt</span></div>
-                            <div>
+                            <!-- <div>
                                 <div id="switch">
                                     <div id="scroll"></div>
                                 </div><span>Nền tối</span>
-                            </div>
+                            </div> -->
                         </div>
                     </div>
                     <div id="search" class="col-sm-4">
@@ -339,7 +376,7 @@
 <script src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons.js"></script>
 <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
 <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
-
+<script src="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.4.2/css/fontawesome.min.css"></script>
 
 <script>
     $(document).ready(function () {
