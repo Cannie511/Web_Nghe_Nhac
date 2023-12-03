52b9d3615b02fa8980258de86d1ce073ffe2d7e2
Cannie511 <Canh51102@gmail.com>
2023-12-04 05:40:07 +0700

    sync

--- AdminPages/page/Admin.php
+++ AdminPages/page/Admin.php
@@ -1,16 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
-<?php include "MusicProcess.php" ?>
-
+<?php include "G:\git_DoAn_WebNgheNhac\Web_Nghe_Nhac\MusicProcess.php" ?>
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="icon" href="IMAGE/user-cog-solid.svg" />
   <title>TriVie Administrator</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">
-  <<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
-  <link rel="stylesheet" href="Admin.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
   <script src="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css"></script>
+  <link rel="stylesheet" href="style/Admin.css">
 </head>
 <style>
 
