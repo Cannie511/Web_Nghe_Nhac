dd7546d555effcf96cc668e53ddee2cdfb7e78ee
Thanh Nam <ngtnam111@example.com>
2023-12-07 15:06:49 +0700

    Tim kiem + show nhac

--- DB/ketnoi.php
+++ DB/ketnoi.php
@@ -6,7 +6,7 @@ $password = "";
 
 try {
   $conn = new PDO("mysql:host=$servername;dbname=webnghenhac", $username, $password);
-  
+  $conn->query("set names 'utf8' ");
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
  
 } catch(PDOException $e) {
--- xuLyTimKiem.php
+++ xuLyTimKiem.php
@@ -1,6 +1,26 @@
 <?php 
 
+function timNguoiDung($name)
+{
+    include ("DB/ketnoi.php");
 
+$sql = "SELECT Ten_Dang_Nhap FROM nguoi_dung WHERE LEFT(Ten_Dang_Nhap, 3) = '$name'";
+    $stm = $conn->prepare($sql);
+    $stm->execute();
+    $data = $stm->fetchAll(PDO::FETCH_ASSOC);
+    if(count($data) > 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+if(isset($_GET['timKiem']))
+{
+    $name = $_GET["IDsearch"];
+    timNguoiDung($name);
+}
 
-echo "Aaaaaaa";
 ?>
