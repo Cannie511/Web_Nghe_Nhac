6c0aa83de61b6d251baaf2c4a233836a99445feb
Cannie511 <Canh51102@gmail.com>
2023-12-09 23:31:51 +0700

    merge

--- index.js
+++ index.js
@@ -2,16 +2,6 @@ $(document).ready(function () {
   var backBtn = 0;
   $("#main_play").scroll(function () {
     if ($(this).scrollTop()) {
-<<<<<<< HEAD
-      $("#topMenu").css("background-color", "var(--primary-color-custom)");
-      $("#topMenu").css("box-shadow", "1px 2px 5px black");
-      $("#topMenu").css("z-index", "3000");
-      $("#personal").css("color", "black");
-    } else {
-      $("#topMenu").css("background-color", "transparent");
-      $("#personal").css("color", "white");
-      $("#topMenu").css("box-shadow", "0 0 0 white");
-=======
       $('#topMenu').css('background-color', 'var(--primary-color-custom)');
       $('#topMenu').css('box-shadow', '1px 2px 5px black');
       $('#personal').css('color', 'black');
@@ -20,7 +10,6 @@ $(document).ready(function () {
       $('#topMenu').css('background-color', 'transparent');
       $('#personal').css('color', 'white');
       $('#topMenu').css('box-shadow', '0 0 0 white');
->>>>>>> c7692c09617838f290b2e66e64c7597211ea9f32
     }
   });
   $(".menu_item").click(function () {
@@ -157,48 +146,6 @@ $(document).ready(function () {
   });
   $("#pause_btn").click(function () {
     music.pause();
-<<<<<<< HEAD
-  });
-  $("#newPlaylist").click(function () {
-    $("#alert-login").css("display", "block");
-    setTimeout(function () {
-      $("#alert-login").css("display", "none");
-    }, 2500);
-  });
-});
-var navbar_toggle = document.getElementById("navbar");
-var menu = document.getElementById("menu");
-var all = document.querySelector("*");
-var main_play = document.getElementById("main_play");
-var logoT = document.getElementById("logo_T");
-var logoV = document.getElementById("logo_V");
-var per = document.getElementById("personal");
-var more = document.getElementById("more");
-var mode = document.getElementById("scroll");
-var night = document.querySelector("body");
-var turn = document.getElementById("switch");
-var search = document.getElementById("search");
-var btn_play_light = document.getElementById("btn");
-var time_play = document.getElementById("time_play");
-var list_music = document.getElementById("list_mp3");
-var logori = document.getElementById("logo_ri");
-var logoie = document.getElementById("logo_ie");
-var logo = document.getElementById("logo");
-var nav_rotate = document.getElementById("nav_rotate");
-var topMenu = document.getElementById("topMenu");
-var menu_cover = document.getElementById("menu_cover");
-if (navbar_toggle != null) {
-  navbar_toggle.onclick = function () {
-    navbar_toggle.classList.toggle("open");
-    menu.classList.toggle("hide");
-    main_play.classList.toggle("full");
-    logoT.classList.toggle("hide");
-    logoV.classList.toggle("hide");
-    logori.classList.toggle("hide");
-    logoie.classList.toggle("hide");
-    logo.classList.toggle("hide");
-    search.classList.toggle("full_scr");
-=======
   })
   // $('#newPlaylist').click(function () {
   //   $('#alert-login').css("display", "block");
@@ -241,41 +188,12 @@ if (navbar_toggle != null) {
       logoie.classList.toggle("hide");
       logo.classList.toggle("hide");
       search.classList.toggle("full_scr");
->>>>>>> c7692c09617838f290b2e66e64c7597211ea9f32
 
     topMenu.classList.toggle("hide");
     menu_cover.classList.toggle("hide");
   };
 }
 
-<<<<<<< HEAD
-function play() {
-  var play = document.getElementById("play_btn");
-  var pause = document.getElementById("pause_btn");
-  play.style.display = "none";
-  pause.style.display = "block";
-  pause.style.position = "relative";
-}
-function pause() {
-  var play = document.getElementById("play_btn");
-  var pause = document.getElementById("pause_btn");
-  play.style.display = "block";
-  pause.style.display = "none";
-}
-function showMore() {
-  more.style.display = "block";
-}
-function hideMore() {
-  more.style.display = "none";
-}
-if (mode != null) {
-  mode.onclick = function () {
-    mode.classList.toggle("night");
-    night.classList.toggle("night_body");
-    turn.classList.toggle("night_on");
-    btn_play_light.classList.toggle("light_btn");
-    time_play.classList.toggle("light_time_play");
-=======
   function play() {
     var play = document.getElementById("play_btn");
     var pause = document.getElementById("pause_btn");
@@ -323,42 +241,9 @@ if (mode != null) {
       btn_play_light.classList.toggle("light_btn");
       time_play.classList.toggle("light_time_play");
     };
->>>>>>> c7692c09617838f290b2e66e64c7597211ea9f32
   };
-}
 
-<<<<<<< HEAD
-function log_in() {
-  window.location = "log-in.php";
-}
-function Edit() {
-  window.location = "EditProfile.html";
-}
-function back() {
-  window.history.back();
-}
-function Home() {
-  window.location = "index.php";
-}
-function playList() {
-  window.location = "playlist.html";
-}
-function Register() {
-  window.location = "Register.php";
-}
-var playlist = [
-  "https://vnso-pt-24-tf-a128-zmp3.zmdcdn.me/966012e4e868f2efc96237dedc1145af?authen=exp=1702026755~acl=/966012e4e868f2efc96237dedc1145af/*~hmac=082a75cb29b93bf5b0684f81ff20e085",
-  "https://vnso-pt-14-tf-a128-zmp3.zmdcdn.me/667dfa7991c627e54f506d737e49390a?authen=exp=1702097802~acl=/667dfa7991c627e54f506d737e49390a/*~hmac=6ca85577f75d85335736eb8b27648a8e",
-];
-var currentSongIndex = 0;
-var audioPlayer = document.getElementById("music");
-var loop = false;
 
-function playNext() {
-  currentSongIndex++;
-  if (currentSongIndex >= playlist.length) {
-    currentSongIndex = 0;
-=======
   function log_in() {
     window.location = "log-in.php";
   }
@@ -437,7 +322,6 @@ function increaseListenCount(songId) {
     audioPlayer.src = playlist[currentSongIndex];
     audioPlayer.load();
     audioPlayer.play();
-
   }
   function playPrev() {
     currentSongIndex--;
@@ -466,12 +350,11 @@ function increaseListenCount(songId) {
     audioPlayer.src = playlist[currentSongIndex];
     audioPlayer.load();
     audioPlayer.play();
->>>>>>> c7692c09617838f290b2e66e64c7597211ea9f32
   }
   audioPlayer.src = playlist[currentSongIndex];
   audioPlayer.load();
   audioPlayer.play();
-}
+
 function playPrev() {
   currentSongIndex--;
   if (currentSongIndex < 0) currentSongIndex = 0;
