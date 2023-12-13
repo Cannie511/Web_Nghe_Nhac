4aea5880b6287f3efb4fd27eeae42abd5b26023d
Cannie511 <Canh51102@gmail.com>
2023-12-14 05:42:52 +0700

    sync

--- test.php
+++ test.php
@@ -1,53 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <style>
-    /* Ẩn nút Dừng mặc định */
-    #btnStop {
-      display: none;
-    }
-  </style>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dynamic Content Loading</title>
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
 </head>
 <body>
 
-<!-- Nút Play -->
-<div id="btnPlay" onclick="playAudio()">Play</div>
+    <a href="#" class="dashboard-nav-dropdown-item" id="loadThemNhac">Thêm nhạc mới</a>
 
-<!-- Nút Dừng -->
-<div id="btnStop" onclick="stopAudio()">Dừng</div>
+    <div id="root"></div>
 
-<!-- Đối tượng Audio -->
-<audio id="myAudio" onplay="showStopButton()" onpause="showPlayButton()">
-  <source src="your-audio-file.mp3" type="audio/mp3">
-  Your browser does not support the audio element.
-</audio>
-
-<script>
-  var audio = document.getElementById("myAudio");
-  var btnPlay = document.getElementById("btnPlay");
-  var btnStop = document.getElementById("btnStop");
-
-  function playAudio() {
-    audio.play();
-  }
-
-  function stopAudio() {
-    audio.pause();
-    audio.currentTime = 0;
-  }
-
-  function showStopButton() {
-    btnPlay.style.display = "none";
-    btnStop.style.display = "inline-block";
-  }
-
-  function showPlayButton() {
-    btnPlay.style.display = "inline-block";
-    btnStop.style.display = "none";
-  }
-</script>
+    <script>
+        $(document).ready(function () {
+            // Gán sự kiện click cho thẻ a
+            $("#loadThemNhac").click(function (e) {
+                e.preventDefault();
+                // Sử dụng jQuery load để include nội dung từ file PHP vào div#root
+                $("#root").load("ThemNhac.php");
+            });
+        });
+    </script>
 
 </body>
 </html>
