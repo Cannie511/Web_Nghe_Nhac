9b059fa3431774ef28ef9c6c21516aff3c7cb359
Nghia <nguyenngocnghia112k@gmail.com>
2023-12-14 07:18:27 +0700

    lay xuong

--- Xuly/doiMK.php
+++ Xuly/doiMK.php
@@ -1,49 +1,56 @@
 <?php 
 
-function doiMK($a,$b,$c,$d)
+function doiMK($a, $b, $c, $d)
 {
-    if(isset($_POST['doiMK']))
-    {
-    include('DB/ketnoi.php');
-    $oldpass =$a;
-    $newpass =$b;
-    $cpass = $c;
-    $maND = $d;
+    if (isset($_POST['doiMK'])) {
+        include('DB/ketnoi.php');
+        $oldpass = $a;
+        $newpass = $b;
+        $cpass = $c;
+        $maND = $d;
 
-    $sql = "select Ten_Dang_Nhap  from nguoi_dung where Mat_Khau='$oldpass'";
+        // Check if the old password matches
+        $sql = "SELECT Mat_Khau FROM nguoi_dung WHERE Ma_ND = :maND";
+        $stm = $conn->prepare($sql);
+        $stm->bindParam(':maND', $maND);
+        $stm->execute();
+        $data = $stm->fetch(PDO::FETCH_ASSOC);
 
-    $stm = $conn->prepare($sql);
-    $stm->execute();
-    $data = $stm->fetchAll(PDO::FETCH_ASSOC);
-    if (count($data) > 0) {
-    
-        if($newpass == $cpass)
-        {
-        $sqlUpdatePass = "UPDATE nguoi_dung SET Mat_Khau = :newpass WHERE Ma_ND = :maND";
-        $stmtUpdatePass = $conn->prepare($sqlUpdatePass);
-        $stmtUpdatePass->bindParam(':newpass', $newpass);
-        $stmtUpdatePass->bindParam(':maND', $maND);
-        $stmtUpdatePass->execute();
-        $rowCount = $stmtUpdatePass->rowCount();
+        if ($data) {
+            // Verify if the old password matches
+            if (password_verify($oldpass, $data['Mat_Khau'])) {
+                // Check if new password and confirm password match
+                if ($newpass == $cpass) {
+                    // Hash the new password
+                    $hashedNewPass = password_hash($newpass, PASSWORD_DEFAULT);
 
-        if ($rowCount > 0) {
-            echo "Đổi mật khẩu thành công.";
-        } 
-        
-    }
-        else
-        {
-            echo "Mật khẩu không khớp";
+                    // Update the hashed password in the database
+                    $sqlUpdatePass = "UPDATE nguoi_dung SET Mat_Khau = :newpass WHERE Ma_ND = :maND";
+
+                    $stmtUpdatePass = $conn->prepare($sqlUpdatePass);
+                    $stmtUpdatePass->bindParam(':newpass', $hashedNewPass);
+                    $stmtUpdatePass->bindParam(':maND', $maND);
+                    $stmtUpdatePass->execute();
+                    $rowCount = $stmtUpdatePass->rowCount();
+
+                    if ($rowCount > 0) {
+                        echo "Đổi mật khẩu thành công.";
+
+                        // Retrieve the new password from the form and use it as needed
+                        echo "Mật khẩu mới: $newpass";
+                    } else {
+                        echo "Không thể cập nhật mật khẩu.";
+                    }
+                } else {
+                    echo "Mật khẩu mới không khớp.";
+                }
+            } else {
+                echo "Mật khẩu cũ không đúng.";
+            }
+        } else {
+            echo "Lỗi truy vấn cơ sở dữ liệu.";
         }
     }
-    
-    else
-    {
-        echo "Loi";
-    }
-
-    }
-
 }
 
 ?>
